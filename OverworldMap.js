52598d52532d8f175c73b78992da05ee3dae31db
xndrbrgs <aborgescolon@gmail.com>
2022-09-16 12:52:17 -0400

    frame integrated movement added

--- OverworldMap.js
+++ OverworldMap.js
@@ -28,11 +28,11 @@ window.OverworldMaps = {
             x: utils.withGrid(5),
             y: utils.withGrid(6),
         }),
-        npc1: new Person({
-            x: utils.withGrid(7),
-            y: utils.withGrid(8),
-            src: "/images/characters/people/npc1.png"
-        })
+        // npc1: new Person({
+        //     x: utils.withGrid(7),
+        //     y: utils.withGrid(8),
+        //     src: "/images/characters/people/npc1.png"
+        // })
     }
   },
   Kitchen: {
--- Sprite.js
+++ Sprite.js
@@ -19,29 +19,62 @@ class Sprite {
 
     // Configure Animation and Initial State
     this.animations = config.animations || {
-      idleDown: [[0, 0]],
+      "idle-down": [[0, 0]],
+      "idle-right": [[0, 1]],
+      "idle-up": [[0, 2]],
+      "idle-left": [[0, 3]],
+      "walk-down": [ [1, 0],[0, 0],[3, 0],[0, 0] ],
+      "walk-right": [ [1, 1],[0, 1],[3, 1],[0, 1] ],
+      "walk-up": [ [1, 2],[0, 2],[3, 2],[0, 2] ],
+      "walk-left": [ [1, 3],[0, 3],[3, 3],[0, 3] ]
     };
 
-    this.currentAnimation = config.currentAnimation || "idleDown";
+    this.currentAnimation = "idle-up"  //config.currentAnimation || "idle-down";
     this.currentAnimationFrame = 0;
+
+    // How many game loop frames will show specific sprite position
+    this.animationFrameLimit = config.animationFrameLimit || 16;
+    this.animationFrameProgress =  this.animationFrameLimit;
 
     // Reference the game object
     this.gameObject = config.gameObject;
   }
 
+  get frame() {
+    return this.animations[this.currentAnimation][this.currentAnimationFrame];
+  }
+
+  updateAnimationProgress() {
+    // Downtick frame progress
+    if (this.animationFrameProgress > 0) {
+      this.animationFrameProgress -= 1;
+      return;
+    }
+
+    // Reset the counter
+    this.animationFrameProgress = this.animationFrameLimit;
+    this.currentAnimationFrame += 1;
+
+    if (this.frame === undefined) {
+      this.currentAnimationFrame = 0;
+    }
+  }
+
   draw(ctx) {
     const x = this.gameObject.x - 8;
-    const y = this.gameObject.y - 8;
+    const y = this.gameObject.y - 18;
 
     this.isShadowLoaded && ctx.drawImage(
         this.shadow,
         x,y
     )
 
+    const [frameX, frameY] = this.frame;
+
     this.isLoaded && ctx.drawImage(
       this.image,
-      0, //left cut
-      0, //top cut
+      frameX * 32, //left cut
+      frameY * 32, //top cut
       32, //width of cut
       32, //height of cut
       x,
@@ -49,5 +82,6 @@ class Sprite {
       32,
       32
     );
+    this.updateAnimationProgress();
   }
 }
